0ea53ad556f189abb1c3479e751a04968ee4cf4a
vb27 <vampblood87@gmail.com>
2020-05-05 17:09:18 -0700

    spelling error

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static("public"));
 //   useFindAndModify: false
 // });
 
-var MONGODB_URI = preocess.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI)
 
